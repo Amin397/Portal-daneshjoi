3d7bfacea7d1e03415890b5da7cf3926775830db
Amin <aminkhademi8@gmail.com>
2019-06-08 04:19:17 +0430

    avalin commit kardane portal daneshjoii

---
+++ app/src/main/java/com/example/portaldaneshjo/Fragments/OmorAmozeshi.java
@@ -0,0 +1,27 @@
+package com.example.portaldaneshjo.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.portaldaneshjo.R;
+
+public class OmorAmozeshi extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.omor_amozeshi,container,false);
+        return view;
+
+    }
+}
