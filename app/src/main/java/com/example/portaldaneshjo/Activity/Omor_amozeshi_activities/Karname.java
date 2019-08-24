2e26afa88850d78cd7cd7099c8b8aa15fd362780
Your Name <aminkhademi8@gmail.com>
2019-08-24 08:19:38 -0700

    Karname part completed update 2 shahrivar 98

--- app/src/main/java/com/example/portaldaneshjo/Activity/Omor_amozeshi_activities/Karname.java
+++ app/src/main/java/com/example/portaldaneshjo/Activity/Omor_amozeshi_activities/Karname.java
@@ -3,13 +3,17 @@ package com.example.portaldaneshjo.Activity.Omor_amozeshi_activities;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
 
+import com.example.portaldaneshjo.Adapter.RecyclerView.RecyclerAdapter_Karname;
 import com.example.portaldaneshjo.Adapter.SpinnerAdapter_Nimsal;
 import com.example.portaldaneshjo.Model.Nimsaltahsili;
+import com.example.portaldaneshjo.Model.RecyclerItem_Karname;
 import com.example.portaldaneshjo.R;
 
 import java.util.ArrayList;
@@ -19,6 +23,9 @@ public class Karname extends AppCompatActivity {
     Toolbar toolbar;
     private Spinner spinner;
     private SpinnerAdapter_Nimsal sAdapter;
+    private RecyclerView recyclerView;
+    private RecyclerAdapter_Karname kadapter;
+    private ArrayList<RecyclerItem_Karname> items = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +34,13 @@ public class Karname extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar_karname_id);
         spinner = (Spinner) findViewById(R.id.spn_nimsaltahsili_karname_id);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_karname_id);
 
         toolbar.setTitle("کارنامه");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //اسپینر برای انتخاب نیمسال تحصیلی
 
         final ArrayList<Nimsaltahsili> nimsaltahsili = new ArrayList<>();
         nimsaltahsili.add(new Nimsaltahsili("نیمسال اول 97 - 98"));
@@ -53,6 +63,19 @@ public class Karname extends AppCompatActivity {
 
             }
         });
-        //اسپینر برای انتخاب نیمسال تحصیلی
+
+        kadapter = new RecyclerAdapter_Karname(getApplicationContext(), items);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        showItemsData();
+        recyclerView.setAdapter(kadapter);
+    }
+
+    public void showItemsData(){
+        items.add(new RecyclerItem_Karname("عـادی","احسان زادخوش","مدار منطقی","2154",45,0,3,"15.00"));
+        items.add(new RecyclerItem_Karname("حذف ماده 31","احسان زادخوش","معماری کامپیوتر","254877",24,0,3,"08.00"));
+        items.add(new RecyclerItem_Karname("عـادی","محبوبه مقیمی","برنامه نویسی پیشرفته","171",60,0,3,"20.00"));
+        items.add(new RecyclerItem_Karname("عـادی","مهرداد آزادی","معادلات دیفرانسیل","8585",36,0,3,"12.00"));
+        items.add(new RecyclerItem_Karname("عـادی","پژمان شاه حسینی","مدار الکتریکی","421313",30,0,3,"10.00"));
+        items.add(new RecyclerItem_Karname("عـادی","سروش مبشری","هوش مصنوعی","12120",57,0,3,"19.00"));
     }
 }
